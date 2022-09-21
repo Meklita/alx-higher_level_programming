793ea2f3493e6653df11e71d168ce28650b9e28a
root <mektnz@gmail.com>
2022-09-21 12:03:03 -0700

    t10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first_node = list;
+	listint_t *second_node = list;
+
+	if (!list)
+		return (0);
+	while (first_node && second_node && second_node->next)
+	{
+		first_node = first_node->next;
+		second_node = second_node->next->next;
+		if (first_node == second_node)
+			return (1);
+	}
+	return (0);
+}
