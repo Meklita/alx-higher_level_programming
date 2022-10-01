9eed6d8c26288dc58a6cebb12ddc0c199342e4d2
root <mektnz@gmail.com>
2022-09-30 17:51:16 -0700

    cmmit6

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,5 +6,5 @@ def uniq_add(my_list=[]):
         if item not in res_list:
             res_list.append(item)
     for uniqs in res_list:
-            res += uniqs
+        res += uniqs
     return res
